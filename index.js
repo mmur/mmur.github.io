2eafa1d926a1c16e81e21e136be259abad8f30be
Tserenvaanchig Munkhjargal <mmur@Tserenvaanchigs-MacBook-Pro.local>
2023-12-05 12:47:51 +0800

    test

--- index.js
+++ index.js
@@ -1,89 +1,97 @@
 let contentIndex = -1;
 const contentInfo = [
+    // {
+    //     "id": 0,
+    //     "name": "solir",
+    //     "src": "./videos/vid_solir_01.mp4",
+    //     "type": "video"
+    // },
     {
         "id": 0,
-        "name": "solir",
-        "src": "./videos/vid_solir_01.mp4",
-        "type": "video"
-    },
-    {
-        "id": 1,
-        "name": "tsagaanErveehei",
-        "src": "./videos/vid_tsagaan_erveehei_02.mp4",
-        "type": "video",
-    },
-    {
-        "id": 2,
-        "name": "gadaadErveehei",
-        "src": "./videos/vid_gadaad_erveehei_03.mp4",
-        "type": "video",
-    },
-    {
-        "id": 3,
-        "name": "shuteenTsoh",
-        "src": "./videos/vid_shuteen_tsoh_04.mp4",
-        "type": "video",
-    },
-    {
-        "id": 4,
-        "name": "vansemberuu",
-        "src": "./videos/vid_vansemberuu_05.mp4",
-        "type": "video"
-    },
-    {
-        "id": 5,
-        "name": "bulboo",
-        "src": "./videos/vid_bulboo_06.mp4",
-        "type": "video",
-    },
-    {
-        "id": 6,
-        "name": "daguurNomin",
-        "src": "./videos/vid_daguur_nomin_07.mp4",
-        "type": "video",
-    },
-    {
-        "id": 7,
-        "name": "kheeriinGaluu",
-        "src": "./videos/vid_kheeriin_galuu_08.mp4",
-        "type": "video",
-    },
-    {
-        "id": 8,
-        "name": "numrugtTas",
-        "src": "./videos/vid_numrugt_tas_09.mp4",
-        "type": "video"
-    },
-    {
-        "id": 9,
-        "name": "tsookhorIrves",
-        "src": "./videos/vid_tsookhor_irves_10.mp4",
-        "type": "video",
-    },
-    {
-        "id": 10,
-        "name": "daguurZaraa",
-        "src": "./videos/vid_daguur_zaraa_11.mp4",
-        "type": "video",
-    },
-    {
-        "id": 11,
-        "name": "argali",
-        "src": "./videos/vid_argali_12.mp4",
-        "type": "video",
-    },
-    {
-        "id": 12,
-        "name": "bambaiKhoshuutMogoi",
-        "src": "./videos/vid_b_h_mogoi_13.mp4",
-        "type": "video"
-    },
-    {
-        "id": 13,
-        "name": "gurvel",
-        "src": "./videos/vid_gurvel_14.mp4",
-        "type": "video",
-    },
+        "name": "tarbosaurusBataar",
+        // "src": "./models/scene.gltf",
+        "src": "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/softmind/scene.gltf",
+        "type": "gltf",
+    }
+    
+    // {
+    //     "id": 1,
+    //     "name": "tsagaanErveehei",
+    //     "src": "./videos/vid_tsagaan_erveehei_02.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 2,
+    //     "name": "gadaadErveehei",
+    //     "src": "./videos/vid_gadaad_erveehei_03.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 3,
+    //     "name": "shuteenTsoh",
+    //     "src": "./videos/vid_shuteen_tsoh_04.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 4,
+    //     "name": "vansemberuu",
+    //     "src": "./videos/vid_vansemberuu_05.mp4",
+    //     "type": "video"
+    // },
+    // {
+    //     "id": 5,
+    //     "name": "bulboo",
+    //     "src": "./videos/vid_bulboo_06.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 6,
+    //     "name": "daguurNomin",
+    //     "src": "./videos/vid_daguur_nomin_07.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 7,
+    //     "name": "kheeriinGaluu",
+    //     "src": "./videos/vid_kheeriin_galuu_08.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 8,
+    //     "name": "numrugtTas",
+    //     "src": "./videos/vid_numrugt_tas_09.mp4",
+    //     "type": "video"
+    // },
+    // {
+    //     "id": 9,
+    //     "name": "tsookhorIrves",
+    //     "src": "./videos/vid_tsookhor_irves_10.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 10,
+    //     "name": "daguurZaraa",
+    //     "src": "./videos/vid_daguur_zaraa_11.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 11,
+    //     "name": "argali",
+    //     "src": "./videos/vid_argali_12.mp4",
+    //     "type": "video",
+    // },
+    // {
+    //     "id": 12,
+    //     "name": "bambaiKhoshuutMogoi",
+    //     "src": "./videos/vid_b_h_mogoi_13.mp4",
+    //     "type": "video"
+    // },
+    // {
+    //     "id": 13,
+    //     "name": "gurvel",
+    //     "src": "./videos/vid_gurvel_14.mp4",
+    //     "type": "video",
+    // },
     // {
     //     "id": 14,
     //     "name": "bakh",
@@ -91,12 +99,13 @@ const contentInfo = [
     //     "type": "video",
     // },
 
-    {
-        "id": 14,
-        "name": "tarbosaurusBataar",
-        "src": "./models/scene.gltf",
-        "type": "gltf",
-    },
+    // {
+    //     "id": 14,
+    //     "name": "tarbosaurusBataar",
+    //     // "src": "./models/scene.gltf",
+    //     "src": "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/softmind/scene.gltf",
+    //     "type": "gltf",
+    // },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -121,13 +130,14 @@ document.addEventListener("DOMContentLoaded", () => {
             const newAsset = document.createElement("a-asset-item");
             newAsset.setAttribute("id", content.name);
             newAsset.setAttribute("src", content.src);
-            newAsset.setAttribute("response-type", "arraybuffer");
+            newAsset.setAttribute("crossorigin", "anonymous");
+            // newAsset.setAttribute("response-type", "arraybuffer");
             assetsEl.appendChild(newAsset);
             console.log(newAsset);
         }
     });
     assetsDiv.parentNode.replaceChild(assetsEl, assetsDiv);
-    
+
     // video entities
     contentInfo.forEach(content => {
         if (content.type == "video") {
@@ -178,6 +188,9 @@ document.addEventListener("DOMContentLoaded", () => {
             document.querySelector("#" + content.name).muted = !document.querySelector("#" + content.name).muted;
         }
     });
+
+    console.log(document.querySelector('a-assets').fileLoader);
+
     //     videoEntity.addEventListener("click", event => {
     //         console.log("click0");
 
