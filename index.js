eda453704f12bc2fe7690733fa9109b7bd5abdff
Tserenvaanchig Munkhjargal <mmur@Tserenvaanchigs-MacBook-Pro.local>
2023-12-06 14:46:47 +0800

    deploy demo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let contentIndex = -1;
+let contentIndex = 1;
 
 document.addEventListener("DOMContentLoaded", () => {
     const sceneEl = document.querySelector('a-scene');
@@ -7,35 +7,73 @@ document.addEventListener("DOMContentLoaded", () => {
     //     arSystem = sceneEl.systems["mindar-image-system"];
     // });
 
-    for (let i = 0; i < 3; i++) {
-        var videoEntity = document.getElementById("entity" + i.toString());
+    var eBakh = document.getElementById("entity1");
+    var vBakh = document.querySelector("#bakh");
+    eBakh.addEventListener("targetFound", event => {
+        console.log("found");
+        contentIndex = 1;
+        console.log(bakh);
+        vBakh.play();
+    });
 
-        videoEntity.addEventListener("targetLost", event => {
-            console.log("lost");
-            contentIndex = -1;
-            var video = document.getElementById("video" + i.toString());
-            video.muted = true;
-            video.pause();
-            video.currentTime = 0;
-        });
+    eBakh.addEventListener("targetLost", event => {
+        console.log("lost");
+        contentIndex = -1;
+        vBakh.muted = true;
+        vBakh.pause();
+        vBakh.currentTime = 0;
+    });
 
-        videoEntity.addEventListener("targetFound", event => {
-            console.log(event);
-            if (event.srcElement.id === "entity" + i.toString()) {
-                console.log("video" + i.toString());
-                contentIndex = i;
-                var video = document.getElementById("video" + i.toString());
-                console.log(video);
-                video.muted = true;
-                video.play();
-            }
-        });
-    };
+    var eMogoi = document.getElementById("entity0");
+    var vMogoi = document.querySelector("#bambaiKhoshuutMogoi");
+    eMogoi.addEventListener("targetFound", event => {
+        console.log("found");
+        contentIndex = 0;
+        console.log(bakh);
+        vMogoi.play();
+    });
+
+    eMogoi.addEventListener("targetLost", event => {
+        console.log("lost");
+        contentIndex = -1;
+        vMogoi.muted = true;
+        vMogoi.pause();
+        vMogoi.currentTime = 0;
+    });
+
+    var eGurvel = document.getElementById("entity2");
+    var vGurvel = document.querySelector("#gurvel");
+    eGurvel.addEventListener("targetFound", event => {
+        console.log("found");
+        contentIndex = 2;
+        console.log(bakh);
+        vGurvel.play();
+    });
+
+    eGurvel.addEventListener("targetLost", event => {
+        console.log("lost");
+        contentIndex = -1;
+        vGurvel.muted = true;
+        vGurvel.pause();
+        vGurvel.currentTime = 0;
+    });
 
     window.addEventListener("click", () => {
-        console.log("clicked", contentIndex);
+        console.log("click");
         if (contentIndex > -1) {
-            document.getElementById("video" + contentIndex.toString()).muted = !document.getElementById("video" + contentIndex.toString()).muted;
+            switch(contentIndex) {
+                case 0:
+                    document.querySelector("#bambaiKhoshuutMogoi").muted = !document.querySelector("#bambaiKhoshuutMogoi").muted;
+                    break;
+                case 1:
+                    document.querySelector("#bakh").muted = !document.querySelector("#bakh").muted;
+                    break;
+                case 2:
+                    document.querySelector("#gurvel").muted = !document.querySelector("#gurvel").muted;
+                    break;
+            }
         }
     });
+
+    console.log(document.querySelector('a-assets').fileLoader);
 });
